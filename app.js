a691dd78bf264a9b5b93ffb7e729afc38e48db1a
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2023-02-22 16:11:43 +0100

    chore: setup mongoose connection

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const feedRoutes = require('./routes/feed');
 
@@ -23,4 +26,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
